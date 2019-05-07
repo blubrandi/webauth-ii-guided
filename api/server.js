7eaf86c8ba196bebf6a90203a533e71182849315
blubrandi <blubrandi0@gmail.com>
2019-05-07 10:51:35 -0600

    Required express-session, created sessionConfig object, with notes.  Added server.use for session config.

--- api/server.js
+++ api/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session') // Required express session
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'monster', //by default would be sid ** session ID **.  can be anything.
+  secret: 'keep it secret, keep it safe -gandalf', // adds a layer of security.  can be any string, that's added.
+  cookie: {
+    httpOnly: true, // prevent access from JavaScript code
+    maxAge: 1000 * 60 * 1, //in milliseconds
+    secure: false, // true means only send the cookie over https
+  },
+  resave: false, //resave session even if it didn't change
+  saveUninitialized: true, // create new sessions automatically, make sure to comply with law
+}
+
+server.use(session(sessionConfig)) // <<<<<<
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
